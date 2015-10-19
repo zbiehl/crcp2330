45936b23e50852414ca9ea918b14758cae113e4d
zbiehl <zbiehl@smu.edu>
2015-10-19 10:47:45 -0500

    Fill complete.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -10,6 +10,54 @@
 
 // Put your code here.
 
-@sum -> @16
+(START)
+@SCREEN
+D=A
+@0
+M=D
 
+(CHECK)
+
+@KBD
+D=M
+@BLK
+D;JGT
+@WHT
+D;JEQ
+
+@CHECK
+0;JMP
+
+(BLK)
+@1
+M=-1
+@SWITCH
+
+(WHT)
+@1
 M=0
+@SWITCH
+0;JMP
+
+(SWITCH)
+@1
+D=M
+
+@0
+A=M
+M=D
+
+@0
+D=M+1
+@KBD
+D=A-D
+
+@0
+M=M+1
+A=M
+
+@SWITCH
+D;JGT
+
+@START
+0;JMP
