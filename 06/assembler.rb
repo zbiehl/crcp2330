478e76a89232f897c517ad76cb6bf6403f0ea63a
zbiehl <zbiehl@smu.edu>
2015-12-10 14:03:33 -0600

    assembler complete.

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,41 +1,68 @@
-#! /usr/bin/env ruby
+require_relative 'code'
+require_relative 'parser'
+require_relative 'symbol_table'
+
+class AssemblerException < Exception; end
 
 class Assembler
-  
+
   def initialize(asm_file, hack_file)
+    unless (File.readable?(asm_file.path) && File.writable?(hack_file.path))
+      raise AssemblerException.new('Could not read/write files.')
+    end
     @asm_file = asm_file
-    
-end
+    @hack_file = hack_file
+    @parser = Parser.new(asm_file)
+    @code = Code.new
+    @symbol_table = SymbolTable.new
+  end
+
+  def assemble!
+    assemble_command while @parser.has_more_commands?
+  end
+
+  def assemble_command
+    if @parser.command_type == Parser::C_COMMAND
+      assemble_c_command
+    elsif @parser.command_type == Parser::A_COMMAND
+      assemble_a_command
+    elsif @parser.command_type == Parser::L_COMMAND
+      # Do nothing
+    end
+    @parser.advance
+  end
+
+  def assemble_a_command
+    @hack_file << "0#{@code.constant(@parser.symbol)}\n"
+  end
+
+  def assemble_c_command
+    comp = @code.comp(@parser.comp)
+    dest = @code.dest(@parser.dest)
+    jump = @code.jump(@parser.jump)
+    @hack_file << "111#{comp}#{dest}#{jump}\n"
+  end
+
 end
 
 def args_valid?
-  ARGV[0] && ARGV[0].include?(".asm") && ARGV.length == 1 
+  ARGV[0] && File.extname(ARGV[0]) == '.asm' && ARGV.size == 1
 end
 
-def is_readable?(path)
-    File.readable?(path)
-end
+abort('Usage: ./assembler.rb Prog.asm') unless args_valid?
 
-def hack_filename(asm_filename)
+begin
+  asm_filename = ARGV[0]
   asm_basename = File.basename(asm_filename, '.asm')
-  path = File.split(asm_filename)[0]
-  "#{path}/#{asm_basename}.hack"
-end
-
-unless args_valid?
-  abort("Usage: ./assembler.rb Prog.asm")
-end
-
-asm_filename = ARGV[0]
-
-unless is_readable?(asm_filename)
-  abort("#{asm_filename} not found or is unreadable.")
-end
-
-File.open(asm_filename, 'r') do |asm_file|
-    
-    File.open(hack_filename(asm_filename), 'w') do |hack_file|
+  asm_path = File.split(asm_filename)[0]
+  hack_filename = "#{asm_path}/#{asm_basename}.mine.hack"
+  File.open(asm_filename, 'r') do |asm_file|
+    File.open(hack_filename, 'w') do |hack_file|
       assembler = Assembler.new(asm_file, hack_file)
       assembler.assemble!
+    end
   end
+rescue Exception => e
+  print e.backtrace.join("\n")
+  abort(e.message)
 end
